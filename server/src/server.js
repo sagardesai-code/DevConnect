a0ea396566805a1122e489424848f018056ef8d5
sagardesai-code <desaisagar2003@gmail.com>
2026-08-07 11:04:22 +0530

    Configure Atlas database and deployment settings

--- server/src/server.js
+++ server/src/server.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, () => {
-    console.log("Server is running on port 5000");
+    console.log(`Server is running on port ${PORT}`);
 });
