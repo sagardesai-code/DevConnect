e8843d8bcbe266b1b9c9406a9dfaa2c56dd597a9
sagardesai-code <desaisagar2003@gmail.com>
2026-08-07 11:01:05 +0530

    Configure Atlas database and deployment settings

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,16 +1,16 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/devconnect");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-        console.log("MongoDB Connected Successfully ✅");
-    } catch (error) {
-        console.log("Database Connection Failed ❌");
-        console.log(error.message);
+    console.log("MongoDB Connected Successfully ✅");
+  } catch (error) {
+    console.log("Database Connection Failed ❌");
+    console.log(error.message);
 
-        process.exit(1);
-    }
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
