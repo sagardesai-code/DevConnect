8c3d262fe9b608db88cb75d7c574f708c659456e
sagardesai-code <desaisagar2003@gmail.com>
2026-08-07 11:54:27 +0530

    Update frontend API URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://devconnect-obop.onrender.com/api"
 });
 
 // Attach JWT token to every request
